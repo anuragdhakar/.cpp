0ebee9a868e20ff79b86e0c26660f211a192252d
anuragdhakar <anuragjarariya777@gmail.com>
2025-01-02 23:07:54 +0530

    arm file

---
+++ armstrong.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int num=153;
+    int b=num;
+    int ans=0 ;
+   while(num!=0){
+    int digit=num%10;
+    ans=ans+digit*digit*digit;
+    num=num/10;
+   }
+   if(ans==b){
+    cout<<"armstrong";
+   }
+   else{
+    cout<<"not a armstrong ";
+   }
+
+    
+
+    return 0;
+}
